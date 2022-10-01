4e1fcabc421f6e08d4ef7533ca395d22e17d54fd
Abhishek Doifode <45434232+abhishekdoifode1@users.noreply.github.com>
2022-10-01 13:56:55 +0530

    Create program1.c

---
+++ program1.c
@@ -0,0 +1,25 @@
+/* WAP to check whether the input is a leap year or not
+input 2000
+op leap
+ip 1999
+op not a leap year
+
+*/
+
+#include<stdio.h>
+void main(){
+	int x;
+	printf("Enter Year: \n");
+	scanf("%d", &x);
+
+	if(x%400==0 && x%100!=0){
+		printf("It is a Leap Year \n");
+	}
+	if(x%4==0 && x%100==0){
+		printf("It is a Leap Year \n");
+	}
+	
+	else{
+		printf("It is not a Leap Year \n");
+	}
+}
